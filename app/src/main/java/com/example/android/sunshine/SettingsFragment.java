062ec8847f2be84176f4dc75622c8f276e6aa642
Richard Le Mesurier <richard@glowworm.mobi>
2018-05-17 16:45:02 +0200

    Add blank PreferenceFragment

---
+++ app/src/main/java/com/example/android/sunshine/SettingsFragment.java
@@ -0,0 +1,15 @@
+package com.example.android.sunshine;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by Richard on 17/05/2018.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }
+}
